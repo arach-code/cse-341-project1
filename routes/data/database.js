616a181aa15835e5806ee9c621130648780a489a
arach-code <aarach72@gmail.com>
2025-03-27 22:21:50 +0300

    added.env

---
+++ routes/data/database.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mongoClient = require('mongo').mongoclient;
+
+let database;
+
+const intDb = (callback) => {
+    if (database){
+        console.log('Db is already initialaized!');
+        return callback(null, database);
+       }
+       mongoClient.connect(process.env.MONGODB_URL)
+         .then((client) => {
+            database = clientIn;
+            callback(null,database);
+         })
+         .catch((err) => {
+            callback(err);
+         });
+    
+};
+
+const getDatabase = () => {
+    if (! database) {
+        throw Error('Database not initialized')
+    }
+    return database;
+};
+
+module.exports = {
+    initDb,
+    getDatabase
+};
